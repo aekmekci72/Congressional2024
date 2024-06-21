741bf8c2f13b8332247750a5c1d7aa9c4a3b1064
aekmekci72 <anna.ekmekci@gmail.com>
2024-06-21 15:56:11 -0400

    connecting flask backend

---
+++ congressional24/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/counter', methods=['GET'])
+def get_counter():
+    return jsonify({'message': 'Hello from the backend!'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
